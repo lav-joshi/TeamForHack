84be8d63b57a01ed62d2727ba272f2a4e695acd1
mightymoin <lcs2019006@iiitl.ac.in>
2020-12-02 14:38:04 +0530

    add profile

---
+++ frontend/src/components/AddProfile.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+import './css/addProfile.css';
+import Nav from './Nav';
+
+export default class AddProfile extends Component {
+  render() {
+    return (
+      <React.Fragment>
+      <Nav></Nav>
+      <div className="mt-5" ></div>
+        <div className="container">
+          <form className="form-group modal-content p-5" action="" method="post">
+            <div className="form-row">
+              <div className="form-group col-md-6">
+                <label for="inputEmail4">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="inputEmail4"
+                  placeholder="Email"
+                />
+              </div>
+              <div className="form-group col-md-6">
+                <label for="inputPassword4">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="inputPassword4"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label for="inputAddress">Address</label>
+              <input
+                type="text"
+                className="form-control"
+                id="inputAddress"
+                placeholder="1234 Main St"
+              />
+            </div>
+            <div className="form-group">
+              <label for="inputAddress2">Address 2</label>
+              <input
+                type="text"
+                className="form-control"
+                id="inputAddress2"
+                placeholder="Apartment, studio, or floor"
+              />
+            </div>
+            <div className="form-row">
+              <div className="form-group col-md-6">
+                <label for="inputCity">City</label>
+                <input type="text" className="form-control" id="inputCity" />
+              </div>
+              <div className="form-group col-md-4">
+                <label for="inputState">State</label>
+                <select id="inputState" className="form-control">
+                  <option selected>Choose...</option>
+                  <option>...</option>
+                </select>
+              </div>
+              <div className="form-group col-md-2">
+                <label for="inputZip">Zip</label>
+                <input type="text" className="form-control" id="inputZip" />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  id="gridCheck"
+                />
+                <label className="form-check-label" for="gridCheck">
+                  Check me out
+                </label>
+              </div>
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Sign in
+            </button>
+          </form>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
