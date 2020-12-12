c6aa78696426c31120491cc5abfbe19206eeffcc
mightymoin <lcs2019006@iiitl.ac.in>
2020-12-13 00:46:30 +0530

    hackathon page

--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,25 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const User = require("../models/User");
-const moment = require("moment");
+const express = require('express');
+const bodyParser = require('body-parser');
+const User = require('../models/User');
+const moment = require('moment');
 const router = express.Router();
-const auth = require("../middleware/authuser");
-
+const auth = require('../middleware/authuser');
+const Hackathon = require('../models/Hackathon');
 
 router.use(bodyParser.urlencoded({ extended: true }));
 
-router.get("/dashboard",auth,(req,res)=>{
-    res.render("dashboard",{currentUser:req.user});
+router.get('/dashboard', auth, (req, res) => {
+  res.render('dashboard', { currentUser: req.user });
 });
 
-router.get("/hackathon",auth,(req,res)=>{
-      
+router.get('/hackathons', auth, (req, res) => {
+  Hackathon.find({}, (err, hackathons) => {
+    if (err) {
+      Error(err);
+    } else {
+      res.render('hackathons', { hackathons: hackathons });
+    }
+  });
 });
 
 module.exports = router;
