543b396c3aa6f46d57a4123e1354dcadb42044c3
MightyMoin <lcs2019006@iiitl.ac.in>
2021-10-01 23:39:35 +0530

    auth.js changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,41 +1,41 @@
-const express = require("express");
-const passport = require("passport");
-const User = require("../models/User");
+const express = require('express');
+const passport = require('passport');
+const User = require('../models/User');
 const router = express.Router();
 
 router.get(
-    "/google",
-    passport.authenticate("google", {
-      scope: ["profile", "email"],
-    })
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
 );
 
-router.get("/google/callback",
-passport.authenticate("google", { failureRedirect: "/" }),
-    (req,res)=>{
-      req.session.token = req.user.accessToken[req.user.accessToken.length-1];
-      res.cookie("token",req.session.token);
-      User.findOne({email:req.user.email},async (err,user)=>{
-            if(user){
-                res.redirect("/user/dashboard");
-            }else{
-                res.redirect("/");
-            }
-      });
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    req.session.token = req.user.accessToken[req.user.accessToken.length - 1];
+    res.cookie('token', req.session.token);
+    User.findOne({ email: req.user.email }, async (err, user) => {
+      if (user) {
+        res.redirect('/user/dashboard');
+      } else {
+        res.redirect('/');
+      }
+    });
   }
 );
 
-router.get("/logout", (req,res)=>{
-    User.findOne({ email: req.user.email },async(err,user)=>{
-        user.accessToken.splice(user.accessToken.length-1,1);
-        await user.save();
-    })
-    req.logout();
-    req.session = null;
-    req.token = null;
-    res.cookie("token", "");
-    res.redirect("/");
-    
-}); 
+router.get('/logout', (req, res) => {
+  User.findOne({ email: req.user.email }, async (err, user) => {
+    user.accessToken.splice(user.accessToken.length - 1, 1);
+    await user.save();
+  });
+  req.logout();
+  req.session = null;
+  req.token = null;
+  res.cookie('token', '');
+  res.redirect('/');
+});
 
 module.exports = router;
