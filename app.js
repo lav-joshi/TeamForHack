c5530cf7b34a57f0e81234f27012731e232bc2a6
Ayushi Panth <lit2019073@iiitl.ac.in>
2021-10-02 14:41:19 +0530

    changes in import statements

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 //Importing npm packages
 
 const express = require("express");
+
 const http = require("http");
+
 const cookieParser = require("cookie-parser");
+
 const cookieSession = require("cookie-session");
+
 const bodyParser = require("body-parser");
+
 const methodOverride = require('method-override');
+
 const passport = require("passport");
+
 const keys = require("./config/keys");
+
 const socketio = require("socket.io");
+
 const formatMessage = require('./utils/messages');
+
 const moment  = require("moment");
 
 
 //Importing MongoDB models
+
 require("./db/mongoose");
 const User = require("./models/User");
 const webScraper = require("./db/webScraper");
 
 
 //Importing Routes
+
 const user = require("./routes/user");
 const auth = require("./routes/auth");
 const friend = require("./routes/friend");
@@ -29,6 +41,7 @@ const friend = require("./routes/friend");
 const port = process.env.PORT||3000;
 
 const app = express();
+
 const server=http.createServer(app);
 
 const io = socketio(server);
@@ -67,6 +80,7 @@ app.get("/",(req,res)=>{
       });
     }else{
       User.findOne({email:req.user.email},(err,user)=>{
+        // console.log(err)
           if(err) Error(err);
           if(user){
             res.redirect("/user/dashboard");
@@ -76,6 +90,7 @@ app.get("/",(req,res)=>{
 });
 
 
+// new web socket connection
 
 io.on('connection',(socket)=>{
   
@@ -84,12 +99,12 @@ io.on('connection',(socket)=>{
   // Runs when user joins room 
     socket.on('joinRoom',({user_id})=>{
       socket.join(user_id);
+      // console.log(user_id)
       console.log("Room Joined");
     });
   
   // Listen for the chat message
   socket.on('chatMessage',({msg,friend_id,user_id})=>{
-
     User.findOne({_id:user_id},async(err,user)=>{
       User.findOne({_id:friend_id},async (err,friend)=>{
 
@@ -118,8 +133,9 @@ io.on('connection',(socket)=>{
                 await friend.save();
               }
             });
-
+            // messaging to a friend
             io.to(user_id).emit('message',formatMessage(user_id,msg,friend_id));
+
             io.to(friend_id).emit('message',formatMessage(user_id,msg,user_id));
       });
     });
@@ -132,5 +148,5 @@ io.on('connection',(socket)=>{
 
 
 server.listen(port,()=>{
-    console.log("Server started on the "+ port + "!");
+    console.log("Server started on "+ port + "!");
 });
