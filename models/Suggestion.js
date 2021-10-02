c87b8936d53388a80185b834397f211e80e0fe7c
Ayushi Panth <lit2019073@iiitl.ac.in>
2021-10-02 14:44:30 +0530

    changes in model of hackathon and user

--- models/Suggestion.js
+++ models/Suggestion.js
@@ -1,21 +1,25 @@
 const mongoose = require("mongoose");
 
 const SuggestionSchema = new mongoose.Schema({
+
   name: {
     type: String,
     trim: true,
     required: true,
   },
+
   email: {
     type: String,
     trim: true,
     required: true,
   },
+
   subject: {
     type: String,
     trim: true,
     required: true,
   },
+  
   message:{
     type: String,
     trim:true,
